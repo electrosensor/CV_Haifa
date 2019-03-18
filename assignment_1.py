cafa4e294c2e7aa0e9dca8ac58f23ef05bbe22e3
Electrosensor <khvoles.yura@gmail.com>
2019-03-18 14:37:36 +0200

    adds interactive_segmentation function declaration

--- assignment_1.py
+++ assignment_1.py
@@ -1,21 +1,19 @@
 import numpy
-import cv2
 
 
 def main():
     print("numpy version: " + numpy.__version__)
-	print("open cv version: " + cv2.__version__)
-
 
 
 if __name__ == '__main__':
     main()
 
-	
 
-def interactive_segmentation(nputImage,segmentedImage,segmaskImage):
-#Input
-#nputImage = '<name of input image file>'   #  use JPG images
-#segmentedImage = <name of output image file – image with transparent segment overlay>
-#segmaskImage = '<name of output image file – image with segmentation mask >
-  print("Hello from a function")
+def interactive_segmentation(input_image,segmented_image, seg_mask_image):
+
+    # Input
+    # inputImage = '<name of input image file>'  (use JPG images)
+    # segmentedImage = <name of output image file – image with transparent segment overlay>
+    # seg_mask_image = '<name of output image file – image with segmentation mask >
+
+    print("Hello from a function")
