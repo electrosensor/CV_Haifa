f99cab861c988dd2beab95dff8255483d01b3bce
hadeelbader <hadeelbader@gmail.com>
2019-03-18 13:55:27 +0200

    prints cv version

--- assignment_1.py
+++ assignment_1.py
@@ -1,8 +1,13 @@
 import numpy
+import cv2
+import numpy
+
 
 
 def main():
     print("numpy version: " + numpy.__version__)
+	print("open cv version: " + cv2.__version__)
+
 
 
 if __name__ == '__main__':
